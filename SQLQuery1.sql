2dda969c5c23a2837ede1d275d76d726cb813ba4
שליו מיכאלי <michaelishalev@gmail.com>
2026-03-22 15:22:45 +0200

    tItems ifxed(nor finished)

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tItems] (
+    [itemName]    NVARCHAR (50)  NOT NULL,
+    [itemDesc]     NVARCHAR (50)  NOT NULL,
+    [itemCount]       int  NOT NULL,
+    [itemPrice]        decimal (10,2) NOT NULL
+    
+);
+
